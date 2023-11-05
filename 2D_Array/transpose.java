3508487c72ec490265f0fcef76706304f24f4cd7
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-11-06 00:46:28 +0530

    Add files via upload

---
+++ 2D_Array/transpose.java
@@ -0,0 +1,30 @@
+package TwoD_Array;
+
+public class transpose {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        Transpose(arr);
+    }
+    public static void Transpose(int[][] arr){
+        for (int row=0;row<arr[0].length;row++){
+            for (int col=row+1;col< arr[0].length;col++){
+                int temp=arr[row][col];
+                arr[row][col]=arr[col][row];
+                arr[col][row]=temp;
+            }
+        }
+        for (int i = 0; i < arr.length ; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        }
+    }
+
