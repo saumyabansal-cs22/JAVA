4ae46130dcafba2adc921b32468edc576c3a5817
Saumya Bansal <122472320+saumyabansal-cs22@users.noreply.github.com>
2023-11-13 19:12:13 +0530

    Add files via upload

---
+++ Strings/string_1.java
@@ -0,0 +1,13 @@
+package String;
+
+public class string_1 {
+    public static void main(String[] args) {
+        String str1="Saumya";
+        String str2="niyati";
+        String str3=new String("hello");
+        String str4=new String("beautiful");
+        String str5=str3 +" helloo";
+
+        String str9="yfd"+"kfdkn";
+    }
+}
